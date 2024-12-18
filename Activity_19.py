577b873c921227b790571ffa5e367d4e9f1a0cf6
nixxdx <ajenicogracias@gmail.com>
2024-12-18 22:05:50 +0800

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	renamed:    Activities/Activity_1.py -> Activity_1.py
    	renamed:    Activities/Activity_10.py -> Activity_10.py
    	renamed:    Activities/Activity_11.py -> Activity_11.py
    	renamed:    Activities/Activity_12.py -> Activity_12.py
    	renamed:    Activities/Activity_13.py -> Activity_13.py
    	renamed:    Activities/Activity_14.py -> Activity_14.py
    	renamed:    Activities/Activity_15.py -> Activity_15.py
    	renamed:    Activities/Activity_16.py -> Activity_16.py
    	renamed:    Activities/Activity_17.py -> Activity_17.py
    	renamed:    Activities/Activity_18.py -> Activity_18.py
    	renamed:    Activities/Activity_19.py -> Activity_19.py
    	renamed:    Activities/Activity_2.py -> Activity_2.py
    	renamed:    Activities/Activity_20.py -> Activity_20.py
    	renamed:    Activities/Activity_3.py -> Activity_3.py
    	renamed:    Activities/Activity_4.py -> Activity_4.py
    	renamed:    Activities/Activity_5.py -> Activity_5.py
    	renamed:    Activities/Activity_6.py -> Activity_6.py
    	renamed:    Activities/Activity_7.py -> Activity_7.py
    	renamed:    Activities/Activity_8.py -> Activity_8.py
    	renamed:    Activities/Activity_9.py -> Activity_9.py
    	renamed:    Activities/FinalOutput.py -> FinalOutput.py

--- Activity_19.py
+++ Activity_19.py
@@ -1,3 +1,3 @@
-from Activities.Activity_18 import factorial  
+from Activity_18 import factorial  
 
 print(f"the factorial of 7 is {factorial(7)}")
--- FinalOutput.py
+++ FinalOutput.py
@@ -636,7 +636,7 @@ def activity18():
 
 def activity19():
     
-    from Activities.Activity_18 import factorial  
+    from Activity_18 import factorial  
 
     print(f"the factorial of 7 is {factorial(7)}")
     return
